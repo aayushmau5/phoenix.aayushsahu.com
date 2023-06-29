95ddd6f5cb11cfc5d6a7d99250efdd199e8ddc96
Aayush Kumar Sahu <aayushmau5@gmail.com>
2023-06-29 14:45:34 +0530

    change elixir version in mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Accumulator.MixProject do
     [
       app: :accumulator,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
