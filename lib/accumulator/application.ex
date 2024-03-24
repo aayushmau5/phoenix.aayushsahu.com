aca8a811a3d85f05208daf72b8623cdeb88dad63
Aayush Kumar Sahu <aayushmau5@gmail.com>
2024-03-24 20:51:50 +0530

    re-enable clustering

--- lib/accumulator/application.ex
+++ lib/accumulator/application.ex
@@ -7,7 +7,7 @@ defmodule Accumulator.Application do
 
   @impl true
   def start(_type, _args) do
-    _topologies = [
+    topologies = [
       dns_poll: [
         strategy: Cluster.Strategy.DNSPoll,
         config: [
@@ -26,7 +26,7 @@ defmodule Accumulator.Application do
       # Start Finch
       {Finch, name: Accumulator.Finch},
       # Start libcluster
-      # {Cluster.Supervisor, [topologies, [name: Accumulator.ClusterSupervisor]]},
+      {Cluster.Supervisor, [topologies, [name: Accumulator.ClusterSupervisor]]},
       # Start the Endpoint (http/https)
       AccumulatorWeb.Endpoint,
       AccumulatorWeb.Presence,
@@ -34,7 +34,7 @@ defmodule Accumulator.Application do
       # Start a worker by calling: Accumulator.Worker.start_link(arg)
       # {Accumulator.Worker, arg}
       Accumulator.Scheduler,
-      {Task.Supervisor, name: Accumulator.TaskRunner},
+      {Task.Supervisor, name: Accumulator.TaskRunner}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
