aed1208729d3c0504f4181a647169b5d99099bf1
Aayush Kumar Sahu <aayushmau5@gmail.com>
2025-12-31 01:27:32 +0530

    fix date elapsed

--- lib/accumulator_web/live/year_live/year_live.ex
+++ lib/accumulator_web/live/year_live/year_live.ex
@@ -9,7 +9,7 @@ defmodule AccumulatorWeb.YearLive do
   @impl true
   def mount(_params, _session, socket) do
     year = 2026
-    today = Date.utc_today() |> Date.add(80)
+    today = Date.utc_today()
 
     days_in_year = if Date.leap_year?(Date.new!(year, 1, 1)), do: 366, else: 365
 
